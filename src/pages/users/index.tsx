9d8c0ad79bdb7efa80f030be626e2dbd12d5b048
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2022-03-10 21:54:15 -0400

    Feature de paginação concluída.

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,39 +16,18 @@ import {
   Spinner
 } from "@chakra-ui/react";
 
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination";
 
-import { useQuery } from 'react-query';
 
 import Header from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import Link from "next/link";
-import { useEffect } from "react";
+
+import { useUsers } from "../../services/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
-
-
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric"
-        })
-      }
-    });
-
-    return users;
-  });
-
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -63,7 +42,10 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>}
+            </Heading>
 
             <Link href="/users/create" passHref>
               <Button as="a" size="sm" fontSize="16" colorScheme="pink" leftIcon={<Icon as={RiAddLine} />}>
@@ -109,7 +91,10 @@ export default function UserList() {
                   ))}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination 
+                totalCountOfRegisters={200}
+                currentPage={5}
+                onPageChange={() => {}} />
             </>
           )}
         </Box>
